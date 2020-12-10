52fb3c1242ca678a6797839d16d245aa792ab2b6
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-10 16:08:09 -0500

    Create pdo.php

---
+++ pdo.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = 'sql1.njit.edu';
+$username = 'jc2284';
+$password = 'Reyez2021A1$';
+$dsn = "mysql:host=$hostname;dbname=$username";
+try {
+    $db = new PDO($dsn, $username, $password);
+    echo "Connected successfully<br>";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
