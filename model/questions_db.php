0c49c6701b5447828a6f8b5dd85eaee8b7dd336a
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-10 16:00:27 -0500

    Create questions_db.php

---
+++ model/questions_db.php
@@ -0,0 +1,32 @@
+<?php
+
+function get_users_questions ($userId) {
+    global $db;
+
+    $query = 'SELECT * FROM questions WHERE ownerid = :userId';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':userId', $userId);
+    $statement->execute();
+
+    $questions = $statement->fetchAll();
+    $statement->closeCursor();
+
+    return $questions;
+}
+
+function create_question ($title, $body, $skills, $ownerid) {
+    global $db;
+
+    $query = 'INSERT INTO questions
+                (title, body, skills, ownerid)
+              VALUES
+                (:title, :body, :skills, :ownerid)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':body', $body);
+    $statement->bindValue(':skills', $skills);
+    $statement->bindValue(':ownerid', $ownerid);
+    $statement->execute();
+    $statement->closeCursor();
+
+}
