45a8b86d0af97a50814e6a5aa3f340a250fc1275
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-12 18:12:54 -0500

    Create database.php

---
+++ model/database.php
@@ -0,0 +1,12 @@
+<?php
+$username = 'jc2284';
+$password = 'Reyez2021A1$'
+$dsn = "mysql:host=sql1.njit.edu;dbname=$username";
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include('../errors/database_error.php');
+    exit();
+}
+?>
