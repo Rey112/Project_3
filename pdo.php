cc4914450baeafbafcf4586a2a0b79e336553b95
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-12 18:12:35 -0500

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = 'sql1.njit.edu';
 $username = 'jc2284';
-$password = 'Reyez2021A1$';
+$password = 'Reyez2020A1$';
 $dsn = "mysql:host=$hostname;dbname=$username";
 try {
     $db = new PDO($dsn, $username, $password);
