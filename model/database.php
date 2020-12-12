1843f31e8da5915ba386308f68fe642359489f79
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-12 18:14:41 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
 $username = 'jc2284';
-$password = 'Reyez2021A1$'
+$password = 'Reyez2020A1$';
 $dsn = "mysql:host=sql1.njit.edu;dbname=$username";
 try {
     $db = new PDO($dsn, $username, $password);
