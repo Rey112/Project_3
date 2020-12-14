ff87d47e2b2c33bcd2622ff821a751fb38c7e97c
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-13 19:23:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require('model/database.php');
-require('model/accounts_db.php');
-require('model/questions_db.php');
+require('database.php');
+require('accounts_db.php');
+require('questions_db.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
