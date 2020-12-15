6ae7a5508fe21dd1dc791e0461acc1928f535c4d
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-15 18:41:24 -0500

    Update database.php

--- database.php
+++ database.php
@@ -6,7 +6,7 @@ try {
     $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
-    include('../errors/database_error.php');
+    include('database_error.php');
     exit();
 }
 ?>
