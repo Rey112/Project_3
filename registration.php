fb5aa1d2ee003aa4e8bee13d6a957a1c265998a7
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-15 18:23:11 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -25,6 +25,6 @@
             <input type="text" name="password" id="Password">
             <br><br>
         </div>
-        <div style="text-align: center"><input type="submit" value="Submit"></div>
+        <div style="text-align: center"><input type="submit" value="Submit" name="action"></div>
 
 <?php include('footer.php')?>
