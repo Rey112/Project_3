8c77d83c3422494c8d9dba9318eb2c41c2d4374b
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-15 19:01:06 -0500

    Update accounts_db.php

--- accounts_db.php
+++ accounts_db.php
@@ -17,6 +17,24 @@ function validate_login($email, $password) {
     }
 }
 
+function register_user($firstName, $lastName, $birthday, $email, $password) {
+    global $db;
+    $query = 'INSERT INTO accounts
+                    (email, fname, lname, birthday, password)
+                  VALUES
+                    (:email, :fname, :lname, :birthday, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':fname', $firstName);
+    $statement->bindValue(':lname', $lastName);
+    $statement->bindValue(':birthday', $birthday);
+    $statement->bindValue(':password', $password);
+
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+
 function get_user($userId) {
     global $db;
     $query = 'SELECT * FROM accounts WHERE id = :userId';
